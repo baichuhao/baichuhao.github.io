d8b66d69081d58637781c7e02332f216a2ba2ded
baichuhao <66865530+baichuhao@users.noreply.github.com>
2020-06-26 17:08:11 +0800

    Add files via upload

--- visualcubegui/what.js
+++ visualcubegui/what.js
@@ -1,69 +1,238 @@
 function fc_n(){
-	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "n"
+	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "n";
+	submit_();
 }
 function fc_d(){
-	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "d"
+	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "d";
+	submit_();
 }
 function fc_l(){
-	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "l"
+	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "l";
+	submit_();
 }
 function fc_s(){
-	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "s"
+	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "s";
+	submit_();
 }
 function fc_w(){
-	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "w"
+	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "w";
+	submit_();
 }
 function fc_y(){
-	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "y"
+	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "y";
+	submit_();
 }
 function fc_r(){
-	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "r"
+	document.getElementById("fdccc").value = document.forms["variable"]["fdc_"].value + "r";
+	submit_();
 }
 function fc_o(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "o"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "o";
+	submit_();
 }
 function fc_b(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "b"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "b";
+	submit_();
 }
 function fc_g(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "g"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "g";
+	submit_();
 }
 function fc_m(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "m"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "m";
+	submit_();
 }
 function fc_p(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "p"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "p";
+	submit_();
 }
 function fc_t(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "t"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "t";
+	submit_();
 }
 
 function fd_n(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "n"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "n";
+	submit_();
 }
 function fd_o(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "o"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "o";
+	submit_();
 }
 function fd_t(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "t"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "t";
+	submit_();
 }
 function fd_u(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "u"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "u";
+	submit_();
 }
 function fd_r(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "r"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "r";
+	submit_();
 }
 function fd_f(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "f"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "f";
+	submit_();
 }
 function fd_d(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "d"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "d";
+	submit_();
 }
 function fd_l(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "l"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "l";
+	submit_();
 }
 function fd_b(){
-	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "b"
+	document.forms["variable"]["fdc_"].value = document.forms["variable"]["fdc_"].value + "b";
+	submit_();
+}
+function sizer_t(){
+	document.getElementById("sizerange").value = document.forms["variable"]["size"].value;
+	submit_();
+}
+function sizer_r(){
+	document.getElementById("sizee").value = document.forms["variable"]["sizer"].value;
+	submit_();
+}
+function r_1_r(){
+	document.getElementById("rr__1").value = document.forms["variable"]["r_1r"].value;
+	submit_();
+}
+function r_2_r(){
+	document.getElementById("rr__2").value = document.forms["variable"]["r_2r"].value;
+	submit_();
+}
+function r_3_r(){
+	document.getElementById("rr__3").value = document.forms["variable"]["r_3r"].value;
+	submit_();
+}
+function r__1_r(){
+	document.getElementById("r_1range").value = document.forms["variable"]["r__1"].value;
+	submit_();
+}
+function r__2_r(){
+	document.getElementById("r_2range").value = document.forms["variable"]["r__2"].value;
+	submit_();
+}
+function r__3_r(){
+	document.getElementById("r_3range").value = document.forms["variable"]["r__3"].value;
+	submit_();
+}
+
+function co_r(){
+	document.getElementById("co__r").value = document.forms["variable"]["cor"].value;
+	submit_();
+}
+function fo_r(){
+	document.getElementById("fo__r").value = document.forms["variable"]["forr"].value;
+	submit_();
+}
+function dist_r(){
+	document.getElementById("dist__r").value = document.forms["variable"]["distr"].value;
+	submit_();
+}
+function co__r(){
+	document.getElementById("corange").value = document.forms["variable"]["co"].value;
+	submit_();
+}
+function fo__r(){
+	document.getElementById("forange").value = document.forms["variable"]["fo"].value;
+	submit_();
+}
+function dist__r(){
+	document.getElementById("distrange").value = document.forms["variable"]["dist"].value;
+	submit_();
+}
+function stage_pzl(){
+	if (document.forms["variable"]["pzl"].value == "3"){
+		document.getElementById("fl2").setAttribute("hidden","hidden");
+		document.getElementById("ll2").setAttribute("hidden","hidden");
+		document.getElementById("oll2").setAttribute("hidden","hidden");
+		document.getElementById("fl").removeAttribute("hidden");
+		document.getElementById("f2l").removeAttribute("hidden");
+		document.getElementById("ll").removeAttribute("hidden");
+		document.getElementById("cll").removeAttribute("hidden");
+		document.getElementById("ell").removeAttribute("hidden");
+		document.getElementById("oll").removeAttribute("hidden");
+		document.getElementById("ocll").removeAttribute("hidden");
+		document.getElementById("oell").removeAttribute("hidden");
+		document.getElementById("coll").removeAttribute("hidden");
+		document.getElementById("ocell").removeAttribute("hidden");
+		document.getElementById("wv").removeAttribute("hidden");
+		document.getElementById("vh").removeAttribute("hidden");
+		document.getElementById("els").removeAttribute("hidden");
+		document.getElementById("cls").removeAttribute("hidden");
+		document.getElementById("cmll").removeAttribute("hidden");
+		document.getElementById("cross").removeAttribute("hidden");
+		document.getElementById("f2l_3").removeAttribute("hidden");
+		document.getElementById("f2l_2").removeAttribute("hidden");
+		document.getElementById("f2l_sm").removeAttribute("hidden");
+		document.getElementById("f2l_1").removeAttribute("hidden");
+		document.getElementById("f2b").removeAttribute("hidden");
+		document.getElementById("line").removeAttribute("hidden");
+		document.getElementById("2x2x2").removeAttribute("hidden");
+		document.getElementById("3x3x3").removeAttribute("hidden");
+	} else {
+		if (document.forms["variable"]["pzl"].value == "2"){
+			document.getElementById("fl2").removeAttribute("hidden");
+			document.getElementById("ll2").removeAttribute("hidden");
+			document.getElementById("oll2").removeAttribute("hidden");
+			document.getElementById("fl").setAttribute("hidden","hidden");
+			document.getElementById("ll").setAttribute("hidden","hidden");
+			document.getElementById("oll").setAttribute("hidden","hidden");
+			document.getElementById("f2l").setAttribute("hidden","hidden");
+			document.getElementById("cll").setAttribute("hidden","hidden");
+			document.getElementById("ell").setAttribute("hidden","hidden");
+			document.getElementById("ocll").setAttribute("hidden","hidden");
+			document.getElementById("oell").setAttribute("hidden","hidden");
+			document.getElementById("coll").setAttribute("hidden","hidden");
+			document.getElementById("ocell").setAttribute("hidden","hidden");
+			document.getElementById("wv").setAttribute("hidden","hidden");
+			document.getElementById("vh").setAttribute("hidden","hidden");
+			document.getElementById("els").setAttribute("hidden","hidden");
+			document.getElementById("cls").setAttribute("hidden","hidden");
+			document.getElementById("cmll").setAttribute("hidden","hidden");
+			document.getElementById("cross").setAttribute("hidden","hidden");
+			document.getElementById("f2l_3").setAttribute("hidden","hidden");
+			document.getElementById("f2l_2").setAttribute("hidden","hidden");
+			document.getElementById("f2l_sm").setAttribute("hidden","hidden");
+			document.getElementById("f2l_1").setAttribute("hidden","hidden");
+			document.getElementById("f2b").setAttribute("hidden","hidden");
+			document.getElementById("line").setAttribute("hidden","hidden");
+			document.getElementById("2x2x2").setAttribute("hidden","hidden");
+			document.getElementById("2x2x3").setAttribute("hidden","hidden");
+		} else {
+			document.getElementById("fl2").setAttribute("hidden","hidden");
+			document.getElementById("ll2").setAttribute("hidden","hidden");
+			document.getElementById("oll2").setAttribute("hidden","hidden");
+			document.getElementById("fl").setAttribute("hidden","hidden");
+			document.getElementById("ll").setAttribute("hidden","hidden");
+			document.getElementById("oll").setAttribute("hidden","hidden");
+			document.getElementById("f2l").setAttribute("hidden","hidden");
+			document.getElementById("cll").setAttribute("hidden","hidden");
+			document.getElementById("ell").setAttribute("hidden","hidden");
+			document.getElementById("ocll").setAttribute("hidden","hidden");
+			document.getElementById("oell").setAttribute("hidden","hidden");
+			document.getElementById("coll").setAttribute("hidden","hidden");
+			document.getElementById("ocell").setAttribute("hidden","hidden");
+			document.getElementById("wv").setAttribute("hidden","hidden");
+			document.getElementById("vh").setAttribute("hidden","hidden");
+			document.getElementById("els").setAttribute("hidden","hidden");
+			document.getElementById("cls").setAttribute("hidden","hidden");
+			document.getElementById("cmll").setAttribute("hidden","hidden");
+			document.getElementById("cross").setAttribute("hidden","hidden");
+			document.getElementById("f2l_3").setAttribute("hidden","hidden");
+			document.getElementById("f2l_2").setAttribute("hidden","hidden");
+			document.getElementById("f2l_sm").setAttribute("hidden","hidden");
+			document.getElementById("f2l_1").setAttribute("hidden","hidden");
+			document.getElementById("f2b").setAttribute("hidden","hidden");
+			document.getElementById("line").setAttribute("hidden","hidden");
+			document.getElementById("2x2x2").setAttribute("hidden","hidden");
+			document.getElementById("2x2x3").setAttribute("hidden","hidden");
+		}
+	}
+	submit_();
 }
 function fdcs(){
 	if (document.forms["variable"]["fdc"].value == "fd"){
@@ -73,12 +242,14 @@ function fdcs(){
 		document.getElementById("fcs").removeAttribute("hidden");
 		document.getElementById("fds").setAttribute("hidden","hidden");
 	}
+	submit_();
 }
 function sch1(){
 	if (document.forms["variable"]["sch_1"].value == "#"){
 		document.getElementById("s_1").removeAttribute("disabled");
 	}else{
 		document.getElementById("s_1").setAttribute("disabled","disabled");
+		submit_();
 	}
 }
 function sch2(){
@@ -86,6 +257,7 @@ function sch2(){
 		document.getElementById("s_2").removeAttribute("disabled");
 	}else{
 		document.getElementById("s_2").setAttribute("disabled","disabled");
+		submit_();
 	}
 }
 function sch3(){
@@ -93,6 +265,7 @@ function sch3(){
 		document.getElementById("s_3").removeAttribute("disabled");
 	}else{
 		document.getElementById("s_3").setAttribute("disabled","disabled");
+		submit_();
 	}
 }
 function sch4(){
@@ -100,6 +273,7 @@ function sch4(){
 		document.getElementById("s_4").removeAttribute("disabled");
 	}else{
 		document.getElementById("s_4").setAttribute("disabled","disabled");
+		submit_();
 	}
 }
 function sch5(){
@@ -107,6 +281,7 @@ function sch5(){
 		document.getElementById("s_5").removeAttribute("disabled");
 	}else{
 		document.getElementById("s_5").setAttribute("disabled","disabled");
+		submit_();
 	}
 }
 function sch6(){
@@ -114,7 +289,7 @@ function sch6(){
 		document.getElementById("s_6").removeAttribute("disabled");
 	}else{
 		document.getElementById("s_6").setAttribute("disabled","disabled");
-		
+		submit_();
 	}
 }
 function bgg(){
@@ -122,7 +297,7 @@ function bgg(){
 		document.getElementById("bggg").removeAttribute("disabled");
 	}else{
 		document.getElementById("bggg").setAttribute("disabled","disabled");
-		
+		submit_();
 	}
 }
 function ccc(){
@@ -130,7 +305,7 @@ function ccc(){
 		document.getElementById("cccc").removeAttribute("disabled");
 	}else{
 		document.getElementById("cccc").setAttribute("disabled","disabled");
-		
+		submit_();
 	}
 }
 function acc(){
@@ -138,6 +313,7 @@ function acc(){
 		document.getElementById("ac__").removeAttribute("disabled");
 	}else{
 		document.getElementById("ac__").setAttribute("disabled","disabled");
+		submit_();
 	}
 }
 function submit_() {
